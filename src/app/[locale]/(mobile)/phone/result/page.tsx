3d9e0d8fccfe90623db444169ff493285d6b80e1
aquaman122 <swj981128@gmail.com>
2026-02-06 16:37:31 +0900

    chore: 메타태그 수정

--- src/app/[locale]/(mobile)/phone/result/page.tsx
+++ src/app/[locale]/(mobile)/phone/result/page.tsx
@@ -25,31 +25,11 @@ export default async function ResultPage({ searchParams }: Props) {
           gtag('event', 'conversion', {'send_to': '${conversionLabel}'});
         `}
       </Script>
-      <Script id="meta-pixel" strategy="afterInteractive">
+      <Script id="meta-pixel-event" strategy="afterInteractive">
         {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '865290846335788');
-          fbq('track', 'PageView');
+          fbq('track', 'InitiateCheckout');
         `}
       </Script>
-      <noscript>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          height="1"
-          width="1"
-          style={{ display: 'none' }}
-          src="https://www.facebook.com/tr?id=865290846335788&ev=PageView&noscript=1"
-          alt="facebook pixel"
-        />
-      </noscript>
-
       <Suspense fallback={<div className="min-h-screen bg-white" />}>
         <ResultPageClient />
       </Suspense>
